7b049a9b005e98e38bad271037eb8191693ff15b
unknown <you@exammahersaman576@gmail.com>
2023-08-20 14:38:36 +0500

    while loop added

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+let number = 6;
+let $value = 1;
+while ($value <= 20) {
+    let result = number * $value;
+    console.log(number + "x" + $value + "=" + result);
+    $value++;
+}
---
+++ index.ts
@@ -0,0 +1,9 @@
+let number = 6;
+let $value = 1;
+
+while ($value<=20) {
+    let result:number=number*$value;
+    console.log(number+"x"+$value+"="+result);
+    $value++;
+    
+}
